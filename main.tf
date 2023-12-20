87ed4b8f1adec82d0fa88a0900f03cd9f8e01e5a
Felipe C. Araujo <felipe.s-04@hotmail.com>
2023-12-20 13:07:24 -0300

    create playbook

--- main.tf
+++ main.tf
@@ -17,6 +17,14 @@ resource "aws_instance" "app_server" {
   ami           = "ami-0fc5d935ebf8bc3bc"
   instance_type = "t2.micro"
   key_name      = "alura_key"
+  # user_data = "${file("init.sh")}"
+  # user_data_replace_on_change = true
+  # user_data = <<-EOF
+  #             #!/bin/bash
+  #             cd /home/ubuntu
+  #             echo "<h1>Feito com Terraform</h1>" > index.html
+  #             nohup busybox httpd -f -p 8080 &
+  #             EOF
   tags = {
     Name = "ami-server"
   }
